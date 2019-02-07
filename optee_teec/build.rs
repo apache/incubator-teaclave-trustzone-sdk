1e4f0f3684638d37d1457fdb7a11c76af4230ac8
Mingshen Sun <mssun@mesalock-linux.org>
2019-02-07 11:02:50 -0800

    optee_teec: add build.rs

---
+++ optee_teec/build.rs
@@ -0,0 +1,9 @@
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let optee_client_dir = env::var("OPTEE_CLIENT_DIR").unwrap_or("../optee/optee_client".to_string());
+    let search_path = Path::new(&optee_client_dir).join("/out/export/lib");
+    println!("cargo:rustc-link-search={}", search_path.display());
+    println!("cargo:rustc-link-lib=teec");
+}
