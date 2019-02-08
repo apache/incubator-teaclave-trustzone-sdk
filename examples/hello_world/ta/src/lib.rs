c5a585327baeb6c982e494a3818229d4394f273e
Simon Wan <simonsywan@gmail.com>
2019-02-08 21:29:10 +0000

    examples/hello_world/ta: port hello world example to Rust

--- examples/hello_world/ta/src/lib.rs
+++ examples/hello_world/ta/src/lib.rs
@@ -1,4 +1,40 @@
-pub fn foo() -> u32 {
-    let _v = vec![1, 2, 3];
-    42
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+extern crate optee_utee;
+pub use optee_utee::*;
+
+#[no_mangle]
+pub extern "C" fn TA_CreateEntryPoint() -> TEE_Result {
+    return TEE_SUCCESS;
+}
+
+#[no_mangle]
+pub extern "C" fn TA_DestroyEntryPoint() {
+}
+
+#[no_mangle]
+pub extern "C" fn TA_OpenSessionEntryPoint(_paramTypes: ParamTypes, _params: TEE_Param, _sessionContext: SessionP) -> TEE_Result {
+    return TEE_SUCCESS;
+}
+
+#[no_mangle]
+pub extern "C" fn TA_CloseSessionEntryPoint(_sessionContext: SessionP) {
+}
+
+#[no_mangle]
+pub extern "C" fn TA_InvokeCommandEntryPoint(_sessionContext: SessionP, _commandID: u32, _paramTypes: ParamTypes, _params: &mut [TEE_Param; 4]) -> TEE_Result {
+    match _commandID {
+        0 => {
+            unsafe { _params[0].value.a += 121; }
+        },
+        1 => {
+            unsafe { _params[0].value.a -= 21; }
+        },
+        _ => {
+            return TEE_ERROR_BAD_PARAMETERS;
+        }
+    }
+    return TEE_SUCCESS;
 }
---
+++ examples/hello_world/ta/src/user_ta_header_defines.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * The name of this file must not be modified
+ */
+
+#ifndef USER_TA_HEADER_DEFINES_H
+#define USER_TA_HEADER_DEFINES_H
+
+/* This UUID is generated with uuidgen
+   the ITU-T UUID generator at http://www.itu.int/ITU-T/asn1/uuid.html */
+#define TA_UUID { 0x8abcf200, 0x2450, 0x11e4, \
+		{ 0xab, 0xe2, 0x00, 0x02, 0xa5, 0xd5, 0xc5, 0x1b} }
+
+#define TA_FLAGS				TA_FLAG_EXEC_DDR
+#define TA_STACK_SIZE			(2 * 1024)
+#define TA_DATA_SIZE			(32 * 1024)
+
+#define TA_CURRENT_TA_EXT_PROPERTIES \
+    { "gp.ta.description", USER_TA_PROP_TYPE_STRING, \
+        "Hello World TA" }, \
+    { "gp.ta.version", USER_TA_PROP_TYPE_U32, &(const uint32_t){ 0x0010 } }
+
+#endif /*USER_TA_HEADER_DEFINES_H*/
