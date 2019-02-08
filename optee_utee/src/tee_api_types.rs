89e79873ca59244401da1729928b03c7f9bf3376
Simon Wan <simonsywan@gmail.com>
2019-02-08 21:34:27 +0000

    optee_utee: add more defines to support hello world example

--- optee_utee/src/tee_api_types.rs
+++ optee_utee/src/tee_api_types.rs
@@ -4,34 +4,65 @@ pub type TEE_Result = uint32_t;
 
 #[repr(C)]
 pub struct TEE_UUID {
-    timeLow: uint32_t,
-    timeMid: uint16_t,
-    timeHiAndVersion: uint16_t,
-    clockSeqAndNode: [uint8_t; 8],
+    pub timeLow: uint32_t,
+    pub timeMid: uint16_t,
+    pub timeHiAndVersion: uint16_t,
+    pub clockSeqAndNode: [uint8_t; 8],
 }
+
+
+/*#[repr(C)]
+pub struct ta_head_func_ptr {
+        ptr64 : uint64_t,
+}*/
+
+
+#[repr(C)]
+pub struct ta_head {
+        pub uuid: TEE_UUID,
+        pub stack_size : c_int,
+        pub flags : uint32_t, 
+        pub entry : *mut c_void,//ta_head_func_ptr,
+}
+
+#[repr(C)]
+pub struct user_ta_property {
+        pub name : &'static str, //*mut c_char,
+        pub ta_type : i8, //original define is type, with enum user_ta_prop_type,
+        pub value : *mut c_void,
+}
+
+/**mut c_char*/
+pub fn user_ta_prop_init(c_ptr:&'static str, number:i8, desc:*mut c_void) -> user_ta_property {
+	user_ta_property {name:c_ptr, ta_type:number, value: desc,}
+}
+
 
 #[repr(C)]
 pub struct TEE_Identity {
-    login: uint32_t,
-    uuid: TEE_UUID,
+    pub login: uint32_t,
+    pub uuid: TEE_UUID,
 }
 
 #[derive(Copy, Clone)]
 #[repr(C)]
-struct Memref {
-    buffer: *mut c_void,
-    size: uint32_t,
+pub struct Memref {
+    pub buffer: *mut c_void,
+    pub size: uint32_t,
 }
 
 #[derive(Copy, Clone)]
 #[repr(C)]
-struct Value {
-    a: uint32_t,
-    b: uint32_t,
+pub struct Value {
+    pub a: uint32_t,
+    pub b: uint32_t,
 }
 
 #[repr(C)]
 pub union TEE_Param {
-    memref: Memref,
-    value: Value
+    pub memref: Memref,
+    pub value: Value
 }
+
+pub type ParamTypes = u32;
+pub type SessionP = *mut *mut c_void;
