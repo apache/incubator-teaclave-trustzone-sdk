7775e09f05d32578122362aa3e3179e1629f7701
Mingshen Sun <mssun@mesalock-linux.org>
2019-02-11 14:15:19 -0800

    examples/hello_world/ta: format Rust sources with cargo fmt

--- examples/hello_world/ta/src/lib.rs
+++ examples/hello_world/ta/src/lib.rs
@@ -7,34 +7,41 @@ pub use optee_utee::*;
 
 #[no_mangle]
 pub extern "C" fn TA_CreateEntryPoint() -> TEE_Result {
-    return TEE_SUCCESS;
+    TEE_SUCCESS
 }
 
 #[no_mangle]
-pub extern "C" fn TA_DestroyEntryPoint() {
+pub extern "C" fn TA_DestroyEntryPoint() {}
+
+#[no_mangle]
+pub extern "C" fn TA_OpenSessionEntryPoint(
+    _paramTypes: ParamTypes,
+    _params: TEE_Param,
+    _sessionContext: SessionP,
+) -> TEE_Result {
+    TEE_SUCCESS
 }
 
 #[no_mangle]
-pub extern "C" fn TA_OpenSessionEntryPoint(_paramTypes: ParamTypes, _params: TEE_Param, _sessionContext: SessionP) -> TEE_Result {
-    return TEE_SUCCESS;
-}
+pub extern "C" fn TA_CloseSessionEntryPoint(_sessionContext: SessionP) {}
 
 #[no_mangle]
-pub extern "C" fn TA_CloseSessionEntryPoint(_sessionContext: SessionP) {
-}
-
-#[no_mangle]
-pub extern "C" fn TA_InvokeCommandEntryPoint(_sessionContext: SessionP, _commandID: u32, _paramTypes: ParamTypes, _params: &mut [TEE_Param; 4]) -> TEE_Result {
+pub extern "C" fn TA_InvokeCommandEntryPoint(
+    _sessionContext: SessionP,
+    _commandID: u32,
+    _paramTypes: ParamTypes,
+    _params: &mut [TEE_Param; 4],
+) -> TEE_Result {
     match _commandID {
-        0 => {
-            unsafe { _params[0].value.a += 121; }
+        0 => unsafe {
+            _params[0].value.a += 121;
         },
-        1 => {
-            unsafe { _params[0].value.a -= 21; }
+        1 => unsafe {
+            _params[0].value.a -= 21;
         },
         _ => {
             return TEE_ERROR_BAD_PARAMETERS;
         }
     }
-    return TEE_SUCCESS;
+    TEE_SUCCESS
 }
