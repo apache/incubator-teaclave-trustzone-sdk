cd6e64f43b252ba1ed4bcd89e618a8f190665469
Mingshen Sun <mssun@mesalock-linux.org>
2019-02-11 20:08:41 -0800

    examples/secure_storage: format styles

--- examples/secure_storage/ta/src/lib.rs
+++ examples/secure_storage/ta/src/lib.rs
@@ -3,13 +3,13 @@
 #![allow(non_snake_case)]
 
 extern crate optee_utee;
-pub use optee_utee::*;
 pub use libc::*;
+pub use optee_utee::*;
 use std::ptr;
 
-pub const TA_SECURE_STORAGE_CMD_READ_RAW:u32 = 0;
-pub const TA_SECURE_STORAGE_CMD_WRITE_RAW:u32 = 1;
-pub const TA_SECURE_STORAGE_CMD_DELETE:u32 = 2;
+pub const TA_SECURE_STORAGE_CMD_READ_RAW: u32 = 0;
+pub const TA_SECURE_STORAGE_CMD_WRITE_RAW: u32 = 1;
+pub const TA_SECURE_STORAGE_CMD_DELETE: u32 = 2;
 
 #[no_mangle]
 pub extern "C" fn TA_CreateEntryPoint() -> TEE_Result {
@@ -17,20 +17,27 @@ pub extern "C" fn TA_CreateEntryPoint() -> TEE_Result {
 }
 
 #[no_mangle]
-pub extern "C" fn TA_DestroyEntryPoint() {
-}
+pub extern "C" fn TA_DestroyEntryPoint() {}
 
 #[no_mangle]
-pub extern "C" fn TA_OpenSessionEntryPoint(_paramTypes: ParamTypes, _params: TEE_Param, _sessionContext: SessionP) -> TEE_Result {
+pub extern "C" fn TA_OpenSessionEntryPoint(
+    _paramTypes: ParamTypes,
+    _params: TEE_Param,
+    _sessionContext: SessionP,
+) -> TEE_Result {
     return TEE_SUCCESS;
 }
 
 #[no_mangle]
-pub extern "C" fn TA_CloseSessionEntryPoint(_sessionContext: SessionP) {
-}
+pub extern "C" fn TA_CloseSessionEntryPoint(_sessionContext: SessionP) {}
 
 pub fn delete_object(paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TEE_Result {
-    let exp_paramTypes: uint32_t = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT, TEE_PARAM_TYPE_NONE, TEE_PARAM_TYPE_NONE, TEE_PARAM_TYPE_NONE);
+    let exp_paramTypes: uint32_t = TEE_PARAM_TYPES(
+        TEE_PARAM_TYPE_MEMREF_INPUT,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE,
+    );
     if paramTypes != exp_paramTypes {
         return TEE_ERROR_BAD_PARAMETERS;
     }
@@ -42,7 +49,13 @@ pub fn delete_object(paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TEE_R
         TEE_MemMove(obj_id, params[0].memref.buffer, obj_id_sz);
 
         let mut object: TEE_ObjectHandle = ptr::null_mut();
-        let res =  TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE, obj_id, obj_id_sz, TEE_DATA_FLAG_ACCESS_READ | TEE_DATA_FLAG_ACCESS_WRITE_META, &mut object as *mut _);
+        let res = TEE_OpenPersistentObject(
+            TEE_STORAGE_PRIVATE,
+            obj_id,
+            obj_id_sz,
+            TEE_DATA_FLAG_ACCESS_READ | TEE_DATA_FLAG_ACCESS_WRITE_META,
+            &mut object as *mut _,
+        );
         if res != TEE_SUCCESS {
             //EMSG("Failed to open persistent object, res=0x%08x", res);
             TEE_Free(obj_id);
@@ -50,29 +63,46 @@ pub fn delete_object(paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TEE_R
         }
         TEE_CloseAndDeletePersistentObject1(object);
         TEE_Free(obj_id);
-        return res;   
+        return res;
     }
 }
 
 pub fn create_raw_object(paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TEE_Result {
-    let exp_paramTypes: uint32_t = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,TEE_PARAM_TYPE_MEMREF_INPUT,TEE_PARAM_TYPE_NONE,TEE_PARAM_TYPE_NONE);
+    let exp_paramTypes: uint32_t = TEE_PARAM_TYPES(
+        TEE_PARAM_TYPE_MEMREF_INPUT,
+        TEE_PARAM_TYPE_MEMREF_INPUT,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE,
+    );
     if paramTypes != exp_paramTypes {
         return TEE_ERROR_BAD_PARAMETERS;
     }
-    
-    unsafe { 
+
+    unsafe {
         let obj_id_sz: uint32_t = params[0].memref.size;
         let obj_id: *mut c_void = TEE_Malloc(obj_id_sz, 0);
         if obj_id.is_null() {
             return TEE_ERROR_OUT_OF_MEMORY;
         }
         TEE_MemMove(obj_id, params[0].memref.buffer, obj_id_sz);
-    
+
         let data: *mut c_void = params[1].memref.buffer as *mut c_void;
         let data_sz: uint32_t = params[1].memref.size;
-        let obj_data_flag: uint32_t = TEE_DATA_FLAG_ACCESS_READ | TEE_DATA_FLAG_ACCESS_WRITE | TEE_DATA_FLAG_ACCESS_WRITE_META | TEE_DATA_FLAG_OVERWRITE;                
+        let obj_data_flag: uint32_t = TEE_DATA_FLAG_ACCESS_READ
+            | TEE_DATA_FLAG_ACCESS_WRITE
+            | TEE_DATA_FLAG_ACCESS_WRITE_META
+            | TEE_DATA_FLAG_OVERWRITE;
         let mut object: TEE_ObjectHandle = ptr::null_mut();
-        let mut res: TEE_Result = TEE_CreatePersistentObject(TEE_STORAGE_PRIVATE, obj_id, obj_id_sz, obj_data_flag, TEE_HANDLE_NULL as TEE_ObjectHandle, ptr::null(), 0, &mut object as *mut _);
+        let mut res: TEE_Result = TEE_CreatePersistentObject(
+            TEE_STORAGE_PRIVATE,
+            obj_id,
+            obj_id_sz,
+            obj_data_flag,
+            TEE_HANDLE_NULL as TEE_ObjectHandle,
+            ptr::null(),
+            0,
+            &mut object as *mut _,
+        );
         if res != TEE_SUCCESS {
             //EMSG("TEE_CreatePersistentObject failed 0x%08x", res);
             TEE_Free(obj_id);
@@ -91,8 +121,13 @@ pub fn create_raw_object(paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> T
     }
 }
 
-pub fn read_raw_object (paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TEE_Result {
-    let exp_paramTypes: uint32_t = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,TEE_PARAM_TYPE_MEMREF_OUTPUT,TEE_PARAM_TYPE_NONE,TEE_PARAM_TYPE_NONE);
+pub fn read_raw_object(paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TEE_Result {
+    let exp_paramTypes: uint32_t = TEE_PARAM_TYPES(
+        TEE_PARAM_TYPE_MEMREF_INPUT,
+        TEE_PARAM_TYPE_MEMREF_OUTPUT,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE,
+    );
     if paramTypes != exp_paramTypes {
         return TEE_ERROR_BAD_PARAMETERS;
     }
@@ -108,14 +143,28 @@ pub fn read_raw_object (paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TE
         let data: *mut c_void = params[1].memref.buffer as *mut c_void;
         let data_sz: uint32_t = params[1].memref.size;
         let mut object: TEE_ObjectHandle = ptr::null_mut();
-        let mut res = TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE, obj_id, obj_id_sz, TEE_DATA_FLAG_ACCESS_READ | TEE_DATA_FLAG_SHARE_READ, &mut object as *mut _);
+        let mut res = TEE_OpenPersistentObject(
+            TEE_STORAGE_PRIVATE,
+            obj_id,
+            obj_id_sz,
+            TEE_DATA_FLAG_ACCESS_READ | TEE_DATA_FLAG_SHARE_READ,
+            &mut object as *mut _,
+        );
         if res != TEE_SUCCESS {
             //EMSG("Failed to open persistent object, res=0x%08x", res);
             TEE_Free(obj_id);
             return res;
         }
 
-        let mut object_info: TEE_ObjectInfo = TEE_ObjectInfo{objectType: 0, objectSize: 0, maxObjectSize: 0, objectUsage: 0, dataSize: 0, dataPosition: 0, handleFlags: 0};
+        let mut object_info: TEE_ObjectInfo = TEE_ObjectInfo {
+            objectType: 0,
+            objectSize: 0,
+            maxObjectSize: 0,
+            objectUsage: 0,
+            dataSize: 0,
+            dataPosition: 0,
+            handleFlags: 0,
+        };
         res = TEE_GetObjectInfo1(object, &mut object_info as *mut _);
         let mut read_bytes: uint32_t = 0; //original type: size_t
         if res != TEE_SUCCESS {
@@ -125,7 +174,12 @@ pub fn read_raw_object (paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TE
                 params[1].memref.size = object_info.dataSize;
                 res = TEE_ERROR_SHORT_BUFFER;
             } else {
-                res = TEE_ReadObjectData(object, data, object_info.dataSize, &mut read_bytes as *mut _);
+                res = TEE_ReadObjectData(
+                    object,
+                    data,
+                    object_info.dataSize,
+                    &mut read_bytes as *mut _,
+                );
                 if res != TEE_SUCCESS || read_bytes != object_info.dataSize {
                     //EMSG("TEE_ReadObjectData failed 0x%08x, read %u over %u",res, read_bytes, object_info.dataSize);
                 } else {
@@ -140,14 +194,19 @@ pub fn read_raw_object (paramTypes: uint32_t, params: &mut [TEE_Param; 4]) -> TE
 }
 
 #[no_mangle]
-pub extern "C" fn TA_InvokeCommandEntryPoint(_sessionContext: SessionP, commandID: u32, paramTypes: ParamTypes, params: &mut [TEE_Param; 4]) -> TEE_Result {
+pub extern "C" fn TA_InvokeCommandEntryPoint(
+    _sessionContext: SessionP,
+    commandID: u32,
+    paramTypes: ParamTypes,
+    params: &mut [TEE_Param; 4],
+) -> TEE_Result {
     match commandID {
         TA_SECURE_STORAGE_CMD_WRITE_RAW => {
             return create_raw_object(paramTypes, params);
-        },
+        }
         TA_SECURE_STORAGE_CMD_READ_RAW => {
             return read_raw_object(paramTypes, params);
-        },
+        }
         TA_SECURE_STORAGE_CMD_DELETE => {
             return delete_object(paramTypes, params);
         }
