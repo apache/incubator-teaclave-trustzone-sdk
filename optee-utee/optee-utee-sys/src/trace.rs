d38360285d3c2613bada7cc9eb40f31e7235af9b
Mingshen Sun <mssun@mesalock-linux.org>
2019-02-13 16:11:28 -0800

    optee-utee: add dhex_dump to the bindings

--- optee-utee/optee-utee-sys/src/trace.rs
+++ optee-utee/optee-utee-sys/src/trace.rs
@@ -6,4 +6,5 @@ extern "C" {
     pub fn trace_set_level(level: c_int);
     pub fn trace_get_level() -> c_int;
     pub fn trace_printf(func: *const c_char, line: c_int, level: c_int, level_ok: bool, fmt: *const c_char, ...);
+    pub fn dhex_dump(function: *const c_char, line: c_int, level: c_int, buf: *const c_void, len: c_int);
 }
