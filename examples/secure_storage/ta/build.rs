27de92f53cc775c68cca051af86122d7666475de
Simon Wan <simonsywan@gmail.com>
2019-02-28 21:18:54 +0000

    Update TA build script with simpler file operation

--- examples/secure_storage/ta/build.rs
+++ examples/secure_storage/ta/build.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::fs::File;
-use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
@@ -9,26 +8,30 @@ use uuid::Uuid;
 fn main() {
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
-    File::create(out.join("user_ta_header.rs"))
-        .unwrap()
-        .write_all(include_bytes!("ta_static.rs"))
-        .unwrap();
+    let mut m_file = File::create(out.join("user_ta_header.rs")).unwrap();
+    let _ = m_file.write_all(include_bytes!("ta_static.rs")).unwrap();
 
-    let mut m_file = OpenOptions::new()
-        .append(true)
-        .open(out.join("user_ta_header.rs"))
-        .unwrap();
-    let _ = writeln!(m_file, "");
+    let _ = m_file.write_fmt(format_args!("\n"));
     let _ = m_file.write_all(include_bytes!("../command_id.rs"));
 
-    let teeuuid = Uuid::parse_str(&include_str!("../uuid.txt").trim()).unwrap();
-    let _ = writeln!(m_file, "");
-    let _ = writeln!(m_file, "const TA_UUID: TEE_UUID = TEE_UUID {{");
-    let _ = writeln!(m_file, "   timeLow: {:#x},", teeuuid.as_fields().0);
-    let _ = writeln!(m_file, "   timeMid: {:#x},", teeuuid.as_fields().1);
-    let _ = writeln!(m_file, "   timeHiAndVersion: {:#x},", teeuuid.as_fields().2);
-    let _ = writeln!(m_file, "   clockSeqAndNode: {:#x?},", teeuuid.as_fields().3);
-    let _ = writeln!(m_file, "}};");
+    macro_rules! uuid_str {
+        () => {
+            "const TA_UUID: TEE_UUID = TEE_UUID {{
+    timeLow: {:#x},
+    timeMid: {:#x},
+    timeHiAndVersion: {:#x},
+    clockSeqAndNode: {:#x?},
+}};"
+        };
+    };
+
+    let tee_uuid = Uuid::parse_str(&include_str!("../uuid.txt").trim()).unwrap();
+    let (time_low, time_mid, time_hi_and_version, clock_seq_and_node) = tee_uuid.as_fields();
+    let _ = m_file.write_fmt(format_args!("\n"));
+    let _ = m_file.write_fmt(format_args!(
+        uuid_str!(),
+        time_low, time_mid, time_hi_and_version, clock_seq_and_node
+    ));
 
     File::create(out.join("ta.lds"))
         .unwrap()
