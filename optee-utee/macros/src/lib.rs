5f881ad829ee066feecc861ae3f7e9624ea3473f
Mingshen Sun <mssun@mesalock-linux.org>
2019-03-15 11:41:03 -0700

    optee-utee: update macros to support session context

--- optee-utee/macros/src/lib.rs
+++ optee-utee/macros/src/lib.rs
@@ -52,81 +52,198 @@ pub fn ta_destroy(_args: TokenStream, input: TokenStream) -> TokenStream {
 /// # Examples
 /// ``` no_run
 /// #[ta_open_session]
-/// fn open_session(_params: &mut Parameters, _sess_ctx: *mut *mut libc::c_void) -> Result<()> { }
+/// fn open_session(params: &mut Parameters, sess_ctx: *mut *mut T) -> Result<()> { }
+///
+/// #[ta_open_session]
+/// fn open_session(params: &mut Parameters) -> Result<()> { }
 /// ```
 #[proc_macro_attribute]
 pub fn ta_open_session(_args: TokenStream, input: TokenStream) -> TokenStream {
     let f = parse_macro_input!(input as syn::ItemFn);
     let ident = &f.ident;
 
-    quote!(
-        #[no_mangle]
-        pub extern "C" fn TA_OpenSessionEntryPoint(
-            param_types: libc::uint32_t,
-            params: &mut [optee_utee_sys::TEE_Param; 4],
-            sess_ctx: *mut *mut libc::c_void,
-        ) -> optee_utee_sys::TEE_Result {
-            let mut parameters = Parameters::new(params, param_types);
-            match #ident(&mut parameters, sess_ctx) {
-                Ok(_) => optee_utee_sys::TEE_SUCCESS,
-                Err(e) => e.raw_code()
+    match f.decl.inputs.len() {
+        1 => quote!(
+            #[no_mangle]
+            pub extern "C" fn TA_OpenSessionEntryPoint(
+                param_types: libc::uint32_t,
+                params: &mut [optee_utee_sys::TEE_Param; 4],
+                sess_ctx: *mut *mut libc::c_void,
+            ) -> optee_utee_sys::TEE_Result {
+                let mut parameters = Parameters::new(params, param_types);
+                match #ident(&mut parameters) {
+                    Ok(_) => optee_utee_sys::TEE_SUCCESS,
+                    Err(e) => e.raw_code()
+                }
             }
-        }
 
-        #f
-    )
-    .into()
+            #f
+        )
+        .into(),
+        2 => {
+            let input_types: Vec<_> = f
+                .decl
+                .inputs
+                .iter()
+                .map(|arg| match arg {
+                    &syn::FnArg::Captured(ref val) => &val.ty,
+                    _ => unreachable!(),
+                })
+                .collect();
+            let t = input_types.last().unwrap();
+
+            quote!(
+                #[no_mangle]
+                pub extern "C" fn TA_OpenSessionEntryPoint(
+                    param_types: libc::uint32_t,
+                    params: &mut [optee_utee_sys::TEE_Param; 4],
+                    sess_ctx: *mut *mut libc::c_void,
+                ) -> optee_utee_sys::TEE_Result {
+                    let mut parameters = Parameters::new(params, param_types);
+                    match #ident(&mut parameters, sess_ctx as #t) {
+                        Ok(_) => optee_utee_sys::TEE_SUCCESS,
+                        Err(e) => e.raw_code()
+                    }
+                }
+
+                #f
+            )
+            .into()
+        }
+        _ => unreachable!(),
+    }
 }
 
 /// # Examples
 /// ``` no_run
 /// #[ta_close_session]
-/// fn close_session(_sess_ctx: &mut Session) { }
+/// fn close_session(sess_ctx: &mut T) { }
+///
+/// #[ta_close_session]
+/// fn close_session() { }
 /// ```
 #[proc_macro_attribute]
 pub fn ta_close_session(_args: TokenStream, input: TokenStream) -> TokenStream {
     let f = parse_macro_input!(input as syn::ItemFn);
     let ident = &f.ident;
+    match f.decl.inputs.len() {
+        0 => quote!(
+            #[no_mangle]
+            pub extern "C" fn TA_CloseSessionEntryPoint(sess_ctx: *mut libc::c_void) {
+                #ident();
+            }
 
-    quote!(
-        #[no_mangle]
-        pub extern "C" fn TA_CloseSessionEntryPoint(sess_ctx: *mut libc::c_void) {
-            let mut sess = Session::new(sess_ctx);
-            #ident(&mut sess)
+            #f
+        )
+        .into(),
+        1 => {
+            let input_types: Vec<_> = f
+                .decl
+                .inputs
+                .iter()
+                .map(|arg| match arg {
+                    &syn::FnArg::Captured(ref val) => &val.ty,
+                    _ => unreachable!(),
+                })
+                .collect();
+            let t = match input_types.first().unwrap() {
+                &syn::Type::Reference(ref r) => &r.elem,
+                _ => unreachable!(),
+            };
+
+            quote!(
+                #[no_mangle]
+                pub extern "C" fn TA_CloseSessionEntryPoint(sess_ctx: *mut libc::c_void) {
+                    if sess_ctx.is_null() {
+                        panic!("sess_ctx is null");
+                    }
+                    let mut b = unsafe {Box::from_raw(sess_ctx as *mut #t)};
+                    #ident(&mut b);
+                    drop(b);
+                }
+
+                #f
+            )
+            .into()
         }
-
-        #f
-    )
-    .into()
+        _ => unreachable!(),
+    }
 }
 
 /// # Examples
 /// ``` no_run
 /// #[ta_invoke_command]
-/// fn invoke_command(_sess_ctx: &mut Session, cmd_id: u32, params: &mut Parameters) -> Result<()> { }
+/// fn invoke_command(sess_ctx: &mut T, cmd_id: u32, params: &mut Parameters) -> Result<()> { }
+///
+/// #[ta_invoke_command]
+/// fn invoke_command(cmd_id: u32, params: &mut Parameters) -> Result<()> { }
 /// ```
 #[proc_macro_attribute]
 pub fn ta_invoke_command(_args: TokenStream, input: TokenStream) -> TokenStream {
     let f = parse_macro_input!(input as syn::ItemFn);
     let ident = &f.ident;
-
-    quote!(
-        #[no_mangle]
-        pub extern "C" fn TA_InvokeCommandEntryPoint(
-            sess_ctx: *mut libc::c_void,
-            cmd_id: u32,
-            param_types: libc::uint32_t,
-            params: &mut [optee_utee_sys::TEE_Param; 4],
-        ) -> optee_utee_sys::TEE_Result {
-            let mut parameters = Parameters::new(params, param_types);
-            let mut sess = Session::new(sess_ctx);
-            match #ident(&mut sess, cmd_id, &mut parameters) {
-                Ok(_) => optee_utee_sys::TEE_SUCCESS,
-                Err(e) => e.raw_code()
+    match f.decl.inputs.len() {
+        2 => quote!(
+            #[no_mangle]
+            pub extern "C" fn TA_InvokeCommandEntryPoint(
+                sess_ctx: *mut libc::c_void,
+                cmd_id: u32,
+                param_types: libc::uint32_t,
+                params: &mut [optee_utee_sys::TEE_Param; 4],
+            ) -> optee_utee_sys::TEE_Result {
+                let mut parameters = Parameters::new(params, param_types);
+                match #ident(cmd_id, &mut parameters) {
+                    Ok(_) => {
+                        optee_utee_sys::TEE_SUCCESS
+                    },
+                    Err(e) => e.raw_code()
+                }
             }
-        }
 
-        #f
-    )
-    .into()
+            #f
+        )
+        .into(),
+        3 => {
+            let input_types: Vec<_> = f
+                .decl
+                .inputs
+                .iter()
+                .map(|arg| match arg {
+                    &syn::FnArg::Captured(ref val) => &val.ty,
+                    _ => unreachable!(),
+                })
+                .collect();
+            let t = match input_types.first().unwrap() {
+                &syn::Type::Reference(ref r) => &r.elem,
+                _ => unreachable!(),
+            };
+
+            quote!(
+                #[no_mangle]
+                pub extern "C" fn TA_InvokeCommandEntryPoint(
+                    sess_ctx: *mut libc::c_void,
+                    cmd_id: u32,
+                    param_types: libc::uint32_t,
+                    params: &mut [optee_utee_sys::TEE_Param; 4],
+                ) -> optee_utee_sys::TEE_Result {
+                    if sess_ctx.is_null() {
+                        return optee_utee_sys::TEE_ERROR_SECURITY;
+                    }
+                    let mut parameters = Parameters::new(params, param_types);
+                    let mut b = unsafe {Box::from_raw(sess_ctx as *mut #t)};
+                    match #ident(&mut b, cmd_id, &mut parameters) {
+                        Ok(_) => {
+                            std::mem::forget(b);
+                            optee_utee_sys::TEE_SUCCESS
+                        },
+                        Err(e) => e.raw_code()
+                    }
+                }
+
+                #f
+            )
+            .into()
+        }
+        _ => unreachable!(),
+    }
 }
