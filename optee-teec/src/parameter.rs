965347c03f2e9e36d091ca1df8ec9520ffb1b577
Mingshen Sun <mssun@mesalock-linux.org>
2019-03-16 18:41:29 -0700

    optee-teec: update parameter related APIs

--- optee-teec/src/parameter.rs
+++ optee-teec/src/parameter.rs
@@ -2,6 +2,37 @@ use optee_teec_sys as raw;
 use std::mem;
 
 #[derive(Copy, Clone)]
+pub struct Parameters(pub Parameter, pub Parameter, pub Parameter, pub Parameter);
+
+impl Parameters {
+    pub fn new(teec_params: [raw::TEEC_Parameter; 4], param_types: u32) -> Self {
+        let (f0, f1, f2, f3) = ParamTypes::from(param_types).into_flags();
+        let p0 = Parameter::from_raw(teec_params[0], f0);
+        let p1 = Parameter::from_raw(teec_params[1], f1);
+        let p2 = Parameter::from_raw(teec_params[2], f2);
+        let p3 = Parameter::from_raw(teec_params[3], f3);
+
+        Parameters(p0, p1, p2, p3)
+    }
+
+    pub fn first(&self) -> &Parameter {
+        &self.0
+    }
+
+    pub fn second(&self) -> &Parameter {
+        &self.1
+    }
+
+    pub fn third(&self) -> &Parameter {
+        &self.2
+    }
+
+    pub fn fourth(&self) -> &Parameter {
+        &self.3
+    }
+}
+
+#[derive(Copy, Clone)]
 pub struct Parameter {
     raw: raw::TEEC_Parameter,
     pub param_type: ParamTypeFlags,
@@ -16,7 +47,7 @@ impl Parameter {
         }
     }
 
-    pub fn value(a: u32, b: u32, param_type: ParamTypeFlags) -> Self {
+    pub fn from_value(a: u32, b: u32, param_type: ParamTypeFlags) -> Self {
         let raw = raw::TEEC_Parameter {
             value: raw::TEEC_Value { a, b },
         };
@@ -26,7 +57,7 @@ impl Parameter {
         }
     }
 
-    pub fn tmpref<T>(buffer: *mut T, size: usize, param_type: ParamTypeFlags) -> Self {
+    pub fn from_tmpref<T>(buffer: *mut T, size: usize, param_type: ParamTypeFlags) -> Self {
         let raw = raw::TEEC_Parameter {
             tmpref: raw::TEEC_TempMemoryReference {
                 buffer: buffer as *mut libc::c_void,
@@ -36,6 +67,19 @@ impl Parameter {
         Self {
             raw: raw,
             param_type: param_type,
+        }
+    }
+
+    pub fn from_raw(raw: raw::TEEC_Parameter, param_type: ParamTypeFlags) -> Self {
+        Self {
+            raw: raw,
+            param_type: param_type,
+        }
+    }
+
+    pub fn value(&self) -> (u32, u32) {
+        unsafe {
+            (self.raw.value.a, self.raw.value.a)
         }
     }
 }
@@ -61,6 +105,25 @@ pub enum ParamTypeFlags {
     MemrefPartialInout = 0xF,
 }
 
+impl From<u32> for ParamTypeFlags {
+    fn from(value: u32) -> Self {
+        match value {
+            0 => ParamTypeFlags::None,
+            1 => ParamTypeFlags::ValueInput,
+            2 => ParamTypeFlags::ValueOutput,
+            3 => ParamTypeFlags::ValueInout,
+            5 => ParamTypeFlags::MemrefTempInput,
+            6 => ParamTypeFlags::MemrefTempOutput,
+            7 => ParamTypeFlags::MemrefTempInout,
+            0xC => ParamTypeFlags::MemrefWhole,
+            0xD => ParamTypeFlags::MemrefPartialInput,
+            0xE => ParamTypeFlags::MemrefPartialOutput,
+            0xF => ParamTypeFlags::MemrefPartialInout,
+            _ => ParamTypeFlags::None,
+        }
+    }
+}
+
 pub struct ParamTypes(u32);
 
 impl ParamTypes {
@@ -71,6 +134,21 @@ impl ParamTypes {
         p3: ParamTypeFlags,
     ) -> Self {
         ParamTypes((p0 as u32) | (p1 as u32) << 4 | (p2 as u32) << 8 | (p3 as u32) << 12)
+    }
+
+    pub fn into_flags(&self) -> (ParamTypeFlags, ParamTypeFlags, ParamTypeFlags, ParamTypeFlags) {
+        (
+            (0x000fu32 & self.0).into(),
+            (0x00f0u32 & self.0).into(),
+            (0x0f00u32 & self.0).into(),
+            (0xf000u32 & self.0).into(),
+        )
+    }
+}
+
+impl From<u32> for ParamTypes {
+    fn from(value: u32) -> Self {
+        ParamTypes(value)
     }
 }
 
