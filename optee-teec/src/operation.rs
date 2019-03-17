e09b5cc6d60a1a2e4c052853152334b2060ecf06
Mingshen Sun <mssun@mesalock-linux.org>
2019-03-16 18:38:05 -0700

    optee-teec: add function to get parameters

--- optee-teec/src/operation.rs
+++ optee-teec/src/operation.rs
@@ -7,21 +7,21 @@ pub struct Operation {
 }
 
 impl Operation {
-    pub fn new(started: u32, param0: Parameter, param1: Parameter, param2: Parameter, param3: Parameter) -> Self {
+    pub fn new(started: u32, p0: Parameter, p1: Parameter, p2: Parameter, p3: Parameter) -> Self {
         let raw_op = raw::TEEC_Operation {
             started: started,
             paramTypes: ParamTypes::new(
-                param0.param_type,
-                param1.param_type,
-                param2.param_type,
-                param3.param_type,
+                p0.param_type,
+                p1.param_type,
+                p2.param_type,
+                p3.param_type,
             )
             .into(),
             params: [
-                param0.into(),
-                param1.into(),
-                param2.into(),
-                param3.into(),
+                p0.into(),
+                p1.into(),
+                p2.into(),
+                p3.into(),
             ],
             session: ptr::null_mut() as *mut raw::TEEC_Session,
         };
@@ -30,5 +30,14 @@ impl Operation {
 
     pub fn as_mut_ptr(&mut self) -> *mut raw::TEEC_Operation {
         &mut self.raw
+    }
+
+    pub fn parameters(&self) -> (Parameter, Parameter, Parameter, Parameter) {
+        let (f0, f1, f2, f3) = ParamTypes::from(self.raw.paramTypes).into_flags();
+        let p0 = Parameter::from_raw(self.raw.params[0], f0);
+        let p1 = Parameter::from_raw(self.raw.params[1], f1);
+        let p2 = Parameter::from_raw(self.raw.params[2], f2);
+        let p3 = Parameter::from_raw(self.raw.params[3], f3);
+        (p0, p1, p2, p3)
     }
 }
