0ed1fbda43d171b97e49d81184688ae3e4ecf660
Mingshen Sun <mssun@mesalock-linux.org>
2019-03-17 14:54:50 -0700

    optee-utee: add documentation

--- optee-utee/src/macros.rs
+++ optee-utee/src/macros.rs
@@ -1,8 +1,35 @@
+/// Macro for printing to the trace output, without a newline.
+///
+/// Equivalent to the `trace_println!` macro expect that a newline is not
+/// printed at the end of the message.
+///
+/// # Examples
+///
+/// ``` no_run
+/// trace_print!("this ")
+/// trace_print!("will ")
+/// trace_print!("be ")
+/// trace_print!("on ")
+/// trace_print!("the ")
+/// trace_print!("same ")
+/// trace_print!("line ")
+/// print!("this string has a newline, why not choose println! instead?\n");
+/// ```
 #[macro_export]
 macro_rules! trace_print {
     ($($arg:tt)*) => ($crate::trace::Trace::_print(format_args!($($arg)*)));
 }
 
+/// Macro for printing to the trace output, with a newline.
+/// Use the `format!` syntax to write data to the standard output. See
+/// `std::fmt` for more information.
+///
+/// # Examples
+///
+/// ``` no_run
+/// trace_println!("Hello, World!");
+/// trace_println!("format {} arguments", "some");
+/// ```
 #[macro_export]
 macro_rules! trace_println {
     () => {
