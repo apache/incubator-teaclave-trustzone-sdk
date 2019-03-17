cf19681682d5df01383da69f8591ed3062828296
Mingshen Sun <mssun@mesalock-linux.org>
2019-03-16 18:43:07 -0700

    optee-utee: public Parameter/ParamTypes struct

--- optee-utee/src/lib.rs
+++ optee-utee/src/lib.rs
@@ -2,7 +2,7 @@ pub use error::{Error, ErrorKind, Result};
 pub use optee_utee_macros::{
     ta_close_session, ta_create, ta_destroy, ta_invoke_command, ta_open_session,
 };
-pub use parameter::{ParamTypeFlags, Parameters};
+pub use parameter::{ParamTypeFlags, Parameters, Parameter, ParamTypes};
 pub use trace::Trace;
 
 #[macro_use]
