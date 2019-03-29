241b5521da7a8cd09cc267e308595fdc3ba984d3
Mingshen Sun <mssun@mesalock-linux.org>
2019-03-29 15:14:24 -0700

    optee-utee: document errors and fix some error messages

--- optee-utee/src/error.rs
+++ optee-utee/src/error.rs
@@ -22,35 +22,67 @@ pub struct Error {
 /// in OP-TEE OS.
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum ErrorKind {
+    /// Object corruption.
     CorruptObject = 0xF0100001,
+    /// Persistent object corruption.
     CorruptObject2 = 0xF0100002,
+    /// Object storage is not available.
     StorageNotAvailable = 0xF0100003,
+    /// Persistent object storage is not available.
     StorageNotAvailable2 = 0xF0100004,
+    /// Non-specific cause.
     Generic = 0xFFFF0000,
+    /// Access privileges are not sufficient.
     AccessDenied = 0xFFFF0001,
+    /// The operation was canceled.
     Cancel = 0xFFFF0002,
+    /// Concurrent accesses caused conflict.
     AccessConflict = 0xFFFF0003,
+    /// Too much data for the requested operation was passed.
     ExcessData = 0xFFFF0004,
+    /// Input data was of invalid format.
     BadFormat = 0xFFFF0005,
+    /// Input parameters were invalid.
     BadParameters = 0xFFFF0006,
+    /// Operation is not valid in the current state.
     BadState = 0xFFFF0007,
+    /// The requested data item is not found.
     ItemNotFound = 0xFFFF0008,
+    /// The requested operation should exist but is not yet implemented.
     NotImplemented = 0xFFFF0009,
+    /// The requested operation is valid but is not supported in this implementation.
     NotSupported = 0xFFFF000A,
+    /// Expected data was missing.
     NoData = 0xFFFF000B,
+    /// System ran out of resources.
     OutOfMemory = 0xFFFF000C,
+    /// The system is busy working on something else.
     Busy = 0xFFFF000D,
+    /// Communication with a remote party failed.
     Communication = 0xFFFF000E,
+    /// A security fault was detected.
     Security = 0xFFFF000F,
+    /// The supplied buffer is too short for the generated output.
     ShortBuffer = 0xFFFF0010,
+    /// The operation has been cancelled by an external event which occurred in
+    /// the REE while the function was in progress.
     ExternalCancel = 0xFFFF0011,
+    /// Data overflow.
     Overflow = 0xFFFF300F,
+    /// Trusted Application has panicked during the operation.
     TargetDead = 0xFFFF3024,
+    /// Insufficient space is available.
     StorageNoSpace = 0xFFFF3041,
+    /// MAC is invalid.
     MacInvalid = 0xFFFF3071,
+    /// Signature is invalid.
     SignatureInvalid = 0xFFFF3072,
+    /// The persistent time has not been set.
     TimeNotSet = 0xFFFF5000,
+    /// The persistent time has been set but may have been corrupted and SHALL
+    /// no longer be trusted.
     TimeNeedsReset = 0xFFFF5001,
+    /// Unknown error.
     Unknown,
 }
 
@@ -83,13 +115,15 @@ impl ErrorKind {
             ErrorKind::Security => "A security fault was detected.",
             ErrorKind::ShortBuffer => "The supplied buffer is too short for the generated output.",
             ErrorKind::ExternalCancel => "Undocumented.",
-            ErrorKind::Overflow => "Data overflow",
+            ErrorKind::Overflow => "Data overflow.",
             ErrorKind::TargetDead => "Trusted Application has panicked during the operation.",
-            ErrorKind::StorageNoSpace => "Storage no space",
-            ErrorKind::MacInvalid => "Mac is invalid.",
+            ErrorKind::StorageNoSpace => "Insufficient space is available.",
+            ErrorKind::MacInvalid => "MAC is invalid.",
             ErrorKind::SignatureInvalid => "Signature is invalid.",
-            ErrorKind::TimeNotSet => "Time is not set.",
-            ErrorKind::TimeNeedsReset => "Time needs to be reset.",
+            ErrorKind::TimeNotSet => "The persistent time has not been set.",
+            ErrorKind::TimeNeedsReset => {
+                "The persistent time has been set but may have been corrupted and SHALL no longer be trusted."
+            },
             ErrorKind::Unknown => "Unknown error.",
         }
     }
