93053ddb7e3499e952eeb69d0e3dcae529bcec7e
Mingshen Sun <mssun@mesalock-linux.org>
2019-04-01 13:36:07 -0700

    ci: try to test the sdk with qemu

---
+++ ci/ci.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -xe
+
+rm -rf screenlog.0
+rm -rf optee-qemuv8-3.4.0
+rm -rf shared
+
+curl http://mesalock-linux.org/assets/optee-qemuv8-3.4.0.tar.gz | tar zxv
+mkdir shared
+cp ../examples/hello_world/ta/target/aarch64-unknown-optee-trustzone/debug/*.ta shared
+cp ../examples/hello_world/host/target/aarch64-unknown-linux-gnu/debug/hello_world shared
+
+screen -L -d -m -S qemu_screen ./optee-qemuv8.sh
+sleep 20
+screen -S qemu_screen -p 0 -X stuff "root\n"
+sleep 5
+screen -S qemu_screen -p 0 -X stuff "mkdir shared && mount -t 9p -o trans=virtio host shared && cd shared\n"
+sleep 5
+screen -S qemu_screen -p 0 -X stuff "cp *.ta /lib/optee_armtz/\n"
+sleep 5
+screen -S qemu_screen -p 0 -X stuff "./hello_world\n"
+sleep 5
+screen -S qemu_screen -p 0 -X stuff "^C"
+sleep 5
+cat screenlog.0
+cat /tmp/serial.log
+grep -q "original value is 29" screenlog.0
+grep -q "inc value is 129" screenlog.0
+grep -q "dec value is 29" screenlog.0
+grep -q "Success" screenlog.0
+
+rm -rf screenlog.0
+rm -rf optee-qemuv8-3.4.0
+rm -rf shared
---
+++ ci/optee-qemuv8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd optee-qemuv8-3.4.0 && ./qemu-system-aarch64 \
+    -nodefaults \
+    -nographic \
+    -serial stdio -serial file:/tmp/serial.log \
+    -smp 2 \
+    -s -machine virt,secure=on -cpu cortex-a57 \
+    -d unimp -semihosting-config enable=on,target=native \
+    -m 1057 \
+    -bios bl1.bin \
+    -initrd rootfs.cpio.gz \
+    -append 'console=ttyAMA0,38400 keep_bootcon root=/dev/vda2' \
+    -kernel Image -no-acpi \
+    -fsdev local,id=fsdev0,path=$(pwd)/../shared,security_model=none -device virtio-9p-device,fsdev=fsdev0,mount_tag=host -netdev user,id=vmnic -device virtio-net-device,netdev=vmnic
