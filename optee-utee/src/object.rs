0e190d02f429493d88e448779599552481d8aa09
Mingshen Sun <mssun@mesalock-linux.org>
2019-04-01 19:27:47 -0700

    optee-utee: document some bit flags

--- optee-utee/src/object.rs
+++ optee-utee/src/object.rs
@@ -6,17 +6,37 @@ use std::mem;
 use std::ptr;
 
 bitflags! {
+    /// A set of flags that controls the access rights and sharing permissions
+    /// with which the object handle is opened.
     pub struct DataFlag: u32 {
+        /// The object is opened with the read access right. This allows the
+        /// Trusted Application to call the function `TEE_ReadObjectData`.
         const ACCESS_READ = 0x00000001;
+        /// The object is opened with the write access right. This allows the
+        /// Trusted Application to call the functions `TEE_WriteObjectData` and
+        /// `TEE_TruncateObjectData`.
         const ACCESS_WRITE = 0x00000002;
+        /// The object is opened with the write-meta access right. This allows
+        /// the Trusted Application to call the functions
+        /// `TEE_CloseAndDeletePersistentObject1` and `TEE_RenamePersistentObject`.
         const ACCESS_WRITE_META = 0x00000004;
+        /// The caller allows another handle on the object to be created with
+        /// read access.
         const SHARE_READ = 0x00000010;
+        /// The caller allows another handle on the object to be created with
+        /// write access.
         const SHARE_WRITE = 0x00000020;
+        /// * If this flag is present and the object exists, then the object is
+        ///   deleted and re-created as an atomic operation: that is, the TA sees
+        ///   either the old object or the new one.
+        /// * If the flag is absent and the object exists, then the function
+        ///   SHALL return `TEE_ERROR_ACCESS_CONFLICT`.
         const OVERWRITE = 0x00000400;
     }
 }
 
 bitflags! {
+    /// A set of flags that defines usages of data in TEE secure storage.
     pub struct UsageFlag: u32 {
         const EXTRACTABLE = 0x00000001;
         const ENCRYPT = 0x00000002;
@@ -106,31 +126,53 @@ pub enum Whence {
 }
 
 pub enum AttributeId {
+    /// Used for all secret keys for symmetric ciphers, MACs, and HMACs
     SecretValue = 0xC0000000,
+    /// RSA modulus: `n`
     RsaModulus = 0xD0000130,
+    /// RSA public key exponent: `e`
     RsaPublicExponent = 0xD0000230,
+    /// RSA private key exponent: `d`
     RsaPrivateExponent = 0xC0000330,
+    /// RSA prime number: `p`
     RsaPrime1 = 0xC0000430,
+    /// RSA prime number: `q`
     RsaPrime2 = 0xC0000530,
+    /// RSA exponent: `dp`
     RsaExponent1 = 0xC0000630,
+    /// RSA exponent: `dq`
     RsaExponent2 = 0xC0000730,
+    /// RSA coefficient: `iq`
     RsaCoefficient = 0xC0000830,
+    /// DSA prime number: `p`
     DsaPrime = 0xD0001031,
+    /// DSA sub prime number: `q`
     DsaSubprime = 0xD0001131,
+    /// DSA base: `g`
     DsaBase = 0xD0001231,
+    /// DSA public value: `y`
     DsaPublicValue = 0xD0000131,
+    /// DSA private value: `x`
     DsaPrivateValue = 0xC0000231,
+    /// Diffie-Hellman prime number: `p`
     DhPrime = 0xD0001032,
+    /// Diffie-Hellman sub prime number: `q`
     DhSubprime = 0xD0001132,
+    /// Diffie-Hellman base: `g`
     DhBase = 0xD0001232,
+    /// Diffie-Hellman x bits: `l`
     DhXBits = 0xF0001332,
+    /// Diffie-Hellman public value: `y`
     DhPublicValue = 0xD0000132,
+    /// Diffie-Hellman public value: `x`
     DhPrivateValue = 0xC0000232,
     RsaOaepLabel = 0xD0000930,
     RsaPssSaltLength = 0xF0000A30,
     EccPublicValueX = 0xD0000141,
     EccPublicValueY = 0xD0000241,
+    /// ECC private value: `d`
     EccPrivateValue = 0xC0000341,
+    /// ECC Curve algorithm
     EccCurve = 0xF0000441,
     BitProtected = (1 << 28),
     BitValue = (1 << 29),
