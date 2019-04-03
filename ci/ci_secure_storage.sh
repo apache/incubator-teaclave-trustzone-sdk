b65cd1587a514338784904b9774f35e96211cad8
Simon Wan <simonsywan@gmail.com>
2019-04-03 11:55:26 -0700

    Update secure storage host with one more case. Add ci test.

---
+++ ci/ci_secure_storage.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -xe
+
+rm -rf screenlog.0
+rm -rf optee-qemuv8-3.4.0
+rm -rf shared
+
+curl http://mesalock-linux.org/assets/optee-qemuv8-3.4.0.tar.gz | tar zxv
+mkdir shared
+cp ../examples/secure_storage/ta/target/aarch64-unknown-optee-trustzone/debug/*.ta shared
+cp ../examples/secure_storage/host/target/aarch64-unknown-linux-gnu/debug/secure_storage shared
+
+screen -L -d -m -S qemu_screen ./optee-qemuv8.sh
+sleep 20
+screen -S qemu_screen -p 0 -X stuff "root\n"
+sleep 5
+screen -S qemu_screen -p 0 -X stuff "mkdir shared && mount -t 9p -o trans=virtio host shared && cd shared\n"
+sleep 5
+screen -S qemu_screen -p 0 -X stuff "cp *.ta /lib/optee_armtz/\n"
+sleep 5
+screen -S qemu_screen -p 0 -X stuff "./secure_storage\n"
+sleep 5
+screen -S qemu_screen -p 0 -X stuff "^C"
+sleep 5
+cat -v screenlog.0
+cat -v /tmp/serial.log
+grep -q "Test on object \"object#1\"" screenlog.0
+grep -q "\- Create and load object in the TA secure storage" screenlog.0
+grep -q "\- Read back the object" screenlog.0
+grep -q "\- Content read-out correctly" screenlog.0
+grep -q "\- Delete the object" screenlog.0
+
+grep -q "Test on object \"object#2\"" screenlog.0
+#miss the read correctly output
+grep -Eq "\- Object not found in TA secure storage, create it|\- Object found in TA secure storage, delete it" screenlog.0
+grep -Eq "\- Create and load object in the TA secure storage|\- Delete the object" screenlog.0
+
+grep -q "We're done, close and release TEE resources" screenlog.0
+
+rm -rf screenlog.0
+rm -rf optee-qemuv8-3.4.0
+rm -rf shared
--- examples/secure_storage/host/src/main.rs
+++ examples/secure_storage/host/src/main.rs
@@ -1,81 +1,120 @@
-use libc::*;
-use optee_teec::{Context, Operation, ParamType, Parameter, Session, Uuid};
+use libc::c_char;
+use optee_teec::{Context, ErrorKind, Operation, ParamType, Parameter, Session, Uuid};
 use std::ffi::CString;
 
 include!(concat!(env!("OUT_DIR"), "/host_header.rs"));
 const TEST_OBJECT_SIZE: usize = 7000;
 
-fn write_secure_object(session: &mut Session, obj_id: &mut CString, obj_data: &mut [c_char]) {
-    let param0 = Parameter::from_tmpref(
+fn write_secure_object(
+    session: &mut Session,
+    obj_id: &mut CString,
+    obj_data: &mut [c_char],
+) -> optee_teec::Result<()> {
+    let p0 = Parameter::from_tmpref(
         obj_id.as_ptr() as *mut c_char,
         obj_id.as_bytes_with_nul().len(),
         ParamType::MemrefTempInput,
     );
-    let param1 = Parameter::from_tmpref(
+    let p1 = Parameter::from_tmpref(
         obj_data.as_mut_ptr(),
         TEST_OBJECT_SIZE,
         ParamType::MemrefTempInput,
     );
-    let param2 = Parameter::new();
-    let param3 = Parameter::new();
-    let mut operation = Operation::new(0, param0, param1, param2, param3);
-    match session.invoke_command(TA_SECURE_STORAGE_CMD_WRITE_RAW, &mut operation) {
-        Ok(_) => println!("Write object to secure storage success."),
-        Err(e) => println!("{:?}", e),
-    }
+    let p2 = Parameter::new();
+    let p3 = Parameter::new();
+    let mut operation = Operation::new(0, p0, p1, p2, p3);
+
+    let _ = session.invoke_command(TA_SECURE_STORAGE_CMD_WRITE_RAW, &mut operation)?;
+
+    println!("- Create and load object in the TA secure storage");
+    Ok(())
 }
 
-fn read_secure_object(session: &mut Session, obj_id: &mut CString, obj_data: &mut [c_char]) {
-    let param0 = Parameter::from_tmpref(
+fn read_secure_object(
+    session: &mut Session,
+    obj_id: &mut CString,
+    obj_data: &mut [c_char],
+) -> optee_teec::Result<()> {
+    let p0 = Parameter::from_tmpref(
         obj_id.as_ptr() as *mut c_char,
         obj_id.as_bytes_with_nul().len(),
         ParamType::MemrefTempInput,
     );
-    let param1 = Parameter::from_tmpref(
+    let p1 = Parameter::from_tmpref(
         obj_data.as_mut_ptr(),
         TEST_OBJECT_SIZE,
         ParamType::MemrefTempOutput,
     );
-    let param2 = Parameter::new();
-    let param3 = Parameter::new();
-    let mut operation = Operation::new(0, param0, param1, param2, param3);
-    match session.invoke_command(TA_SECURE_STORAGE_CMD_READ_RAW, &mut operation) {
-        Ok(_) => println!("Read back object success."),
-        Err(e) => println!("{:?}", e),
-    }
+    let p2 = Parameter::new();
+    let p3 = Parameter::new();
+    let mut operation = Operation::new(0, p0, p1, p2, p3);
+
+    let _ = session.invoke_command(TA_SECURE_STORAGE_CMD_READ_RAW, &mut operation)?;
+
+    println!("- Read back the object");
+    Ok(())
 }
 
-fn delete_secure_object(session: &mut Session, obj_id: &mut CString) {
-    let param0 = Parameter::from_tmpref(
+fn delete_secure_object(session: &mut Session, obj_id: &mut CString) -> optee_teec::Result<()> {
+    let p0 = Parameter::from_tmpref(
         obj_id.as_ptr() as *mut c_char,
         obj_id.as_bytes_with_nul().len(),
         ParamType::MemrefTempInput,
     );
-    let param1 = Parameter::new();
-    let param2 = Parameter::new();
-    let param3 = Parameter::new();
-    let mut operation = Operation::new(0, param0, param1, param2, param3);
-    match session.invoke_command(TA_SECURE_STORAGE_CMD_DELETE, &mut operation) {
-        Ok(_) => println!("Delete object success."),
-        Err(e) => println!("{:?}", e),
-    }
+    let p1 = Parameter::new();
+    let p2 = Parameter::new();
+    let p3 = Parameter::new();
+    let mut operation = Operation::new(0, p0, p1, p2, p3);
+
+    let _ = session.invoke_command(TA_SECURE_STORAGE_CMD_DELETE, &mut operation)?;
+
+    println!("- Delete the object");
+    Ok(())
 }
 
-fn main() -> Result<(), Box<std::error::Error>> {
-    let mut obj_id = CString::new("object#1")?;
-    let mut obj_data = [0xA1u8; TEST_OBJECT_SIZE];
+fn main() -> optee_teec::Result<()> {
+    let mut ctx = Context::new()?;
+    let uuid =
+        Uuid::parse_str(&include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../uuid.txt")).trim())
+            .unwrap();
+    let mut session = ctx.open_session(uuid)?;
+
+    let mut obj1_id = CString::new("object#1").unwrap();
+    let mut obj1_data = [0xA1u8; TEST_OBJECT_SIZE];
     let mut read_data = [0x00u8; TEST_OBJECT_SIZE];
 
-    let uuid =
-        Uuid::parse_str(&include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../uuid.txt")).trim())?;
-    let mut ctx = Context::new()?;
-    let mut session = ctx.open_session(uuid)?;
-    write_secure_object(&mut session, &mut obj_id, &mut obj_data);
-    read_secure_object(&mut session, &mut obj_id, &mut read_data);
+    println!("\nTest on object \"object#1\"");
+    write_secure_object(&mut session, &mut obj1_id, &mut obj1_data)?;
+    read_secure_object(&mut session, &mut obj1_id, &mut read_data)?;
 
-    if obj_data.iter().zip(read_data.iter()).all(|(a, b)| a == b) {
-        println!("obj_data == read_data");
+    if obj1_data.iter().zip(read_data.iter()).all(|(a, b)| a == b) {
+        println!("- Content read-out correctly");
+    } else {
+        println!("- Unexpected content found in secure storage");
     }
-    delete_secure_object(&mut session, &mut obj_id);
+    delete_secure_object(&mut session, &mut obj1_id)?;
+
+    let mut obj2_id = CString::new("object#2").unwrap();
+
+    println!("\nTest on object \"object#2\"");
+    match read_secure_object(&mut session, &mut obj2_id, &mut read_data) {
+        Err(e) => {
+            if e.kind() != ErrorKind::ItemNotFound {
+                println!("{}", e);
+                return Err(e);
+            } else {
+                println!("- Object not found in TA secure storage, create it");
+                let mut obj2_data = [0xB1u8; TEST_OBJECT_SIZE];
+                write_secure_object(&mut session, &mut obj2_id, &mut obj2_data)?;
+            }
+        }
+
+        Ok(()) => {
+            println!("- Object found in TA secure storage, delete it");
+            delete_secure_object(&mut session, &mut obj2_id)?;
+        }
+    }
+
+    println!("\nWe're done, close and release TEE resources");
     Ok(())
 }
