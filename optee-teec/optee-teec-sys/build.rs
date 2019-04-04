16c43811f605ba4eb94242ca0623b526836248c3
Mingshen Sun <mssun@mesalock-linux.org>
2019-04-04 14:09:15 -0700

    optee-teec-sys: fix teec search path

--- optee-teec/optee-teec-sys/build.rs
+++ optee-teec/optee-teec-sys/build.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 
 fn main() {
     let optee_client_dir = env::var("OPTEE_CLIENT_DIR").unwrap_or("../../optee/optee_client".to_string());
-    let search_path = Path::new(&optee_client_dir).join("out/export/lib");
+    let search_path = Path::new(&optee_client_dir).join("out/export/usr/lib");
     println!("cargo:rustc-link-search={}", search_path.display());
     println!("cargo:rustc-link-lib=static=teec");
 }
