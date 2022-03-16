066cc6a87df9b61071e4fba0fb685b096c16f322
Yuan Zhuang <zhuangyuan04@baidu.com>
2022-03-16 07:39:25 +0000

    Remove git submodules
    - Remove git submodules optee/ and rust/
    - Initialize optee/ and rust/ submodules in setup.sh
    - Update ci.yml

--- setup.sh
+++ setup.sh
@@ -17,6 +17,9 @@
 # specific language governing permissions and limitations
 # under the License.
 
+set -xe
+
+##########################################
 # install Rust and select a proper version
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2021-09-20
 source $HOME/.cargo/env
@@ -27,7 +30,84 @@ rustup default 1.44.0 && cargo +1.44.0 install xargo
 # switch to nightly
 rustup default nightly-2021-09-20
 
-# initialize Teaclave TrustZone SDK submodule
-git submodule update --init -- rust
-cd rust/compiler-builtins && git submodule update --init libm
-cd ../rust && git submodule update --init library/stdarch && git submodule update --init library/backtrace
+########################################################
+# initialize submodules: optee_os / optee_client / build
+OPTEE_RELEASE_VERSION=3.16.0
+
+if [[ -z $OPTEE_DIR ]] || [[ $OPTEE_DIR==$PWD/optee ]]
+then
+	OPTEE_DIR=$PWD/optee
+	echo optee dir: $OPTEE_DIR
+	OPTEE_SUBMODULES=("optee_os" "optee_client" "build")
+
+	if [ ! -d $OPTEE_DIR ]
+	then
+		mkdir $OPTEE_DIR
+	else
+		rm -r $OPTEE_DIR/*
+	fi
+
+	# download optee release
+	echo "Downloading optee release..."
+	for submodule in ${OPTEE_SUBMODULES[*]}
+	do
+		echo "Downloading $submodule..."
+		curl --retry 5 -s -S \
+			-L https://github.com/OP-TEE/$submodule/archive/refs/tags/$OPTEE_RELEASE_VERSION.tar.gz \
+			-o $OPTEE_DIR/$OPTEE_RELEASE_VERSION.tar.gz
+		if [ ! $? -eq 0 ]
+		then
+			rm $OPTEE_DIR/$OPTEE_RELEASE_VERSION.tar.gz && \
+				echo "Download failed" && \
+				exit 1
+		fi
+		echo "Uncompressing $submodule..."
+		mkdir -p $OPTEE_DIR/$submodule && \
+			tar zxf $OPTEE_DIR/$OPTEE_RELEASE_VERSION.tar.gz \
+			-C $OPTEE_DIR/$submodule --strip-components 1
+	        if [ ! $? -eq 0 ]
+		then
+			rm $OPTEE_DIR/$OPTEE_RELEASE_VERSION.tar.gz && \
+				rm -r $OPTEE_DIR/$submodule && \
+				echo "Downloaded file is damaged" && \
+				exit 1
+	        fi
+		rm $OPTEE_DIR/$OPTEE_RELEASE_VERSION.tar.gz
+	done
+	echo "Download finished"
+else
+	echo "OPTEE_DIR has been set, omit to download optee submodules"
+fi
+
+
+########################################################
+# initialize submodules: rust / compiler-builtins / libc
+RUST_COMMIT_ID=6abda667852184641149d34da4730d96ba4f7d31
+COMPILER_BUILTINS_COMMIT_ID=45a2e4996fe732172004b292b07397f9a02265ab
+LIBC_COMMIT_ID=1ddfbbbc190bec0f5ec32b08e97585b34d0c6b09
+
+if [ ! -d rust/ ]
+then
+        mkdir rust/
+else
+        rm -r rust/*
+fi
+
+cd rust
+
+git clone https://github.com/mesalock-linux/rust.git && \
+	(cd rust && \
+	git checkout $RUST_COMMIT_ID && \
+	git submodule update --init library/stdarch && \
+	git submodule update --init library/backtrace)
+
+git clone https://github.com/mesalock-linux/compiler-builtins.git && \
+	(cd compiler-builtins && \
+	git checkout $COMPILER_BUILTINS_COMMIT_ID && \
+	git submodule update --init libm)
+
+git clone https://github.com/mesalock-linux/libc.git && \
+	(cd libc && \
+	git checkout $LIBC_COMMIT_ID)
+
+echo "Rust submodules initialized"
