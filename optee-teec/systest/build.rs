f584197a8d6ca8be273d38623fc7f4d804899364
Sumit Garg <sumit.garg@linaro.org>
2023-12-29 18:33:12 +0530

    optee-teec: Use standard OPTEE_CLIENT_EXPORT
    Similar to C counterparts, use OPTEE_CLIENT_EXPORT as a standard path to
    export pre-built optee-client libraries and headers.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>

--- optee-teec/systest/build.rs
+++ optee-teec/systest/build.rs
@@ -21,7 +21,7 @@ fn main() {
     let mut cfg = ctest::TestGenerator::new();
     cfg.target("aarch64-unknown-linux-gnu")
        .header("tee_client_api.h")
-       .include(env::var("OPTEE_CLIENT_INCLUDE").unwrap())
+       .include(env::var("OPTEE_CLIENT_EXPORT").unwrap())
        .type_name(|s, _is_struct, _is_union| {
             s.to_string()
     });
