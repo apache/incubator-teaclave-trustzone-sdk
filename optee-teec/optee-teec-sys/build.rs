d31decd4e042eec744f1474de64385026e8b66ac
Sumit Garg <sumit.garg@linaro.org>
2023-12-29 18:33:12 +0530

    optee-teec: Use standard OPTEE_CLIENT_EXPORT
    Similar to C counterparts, use OPTEE_CLIENT_EXPORT as a standard path to
    export pre-built optee-client libraries and headers.
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>

--- optee-teec/optee-teec-sys/build.rs
+++ optee-teec/optee-teec-sys/build.rs
@@ -19,8 +19,8 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let optee_client_dir = env::var("OPTEE_CLIENT_DIR").unwrap_or("../../optee/optee_client/out".to_string());
-    let search_path = Path::new(&optee_client_dir).join("libteec");
+    let optee_client_dir = env::var("OPTEE_CLIENT_EXPORT").unwrap_or("../optee_client/out/export".to_string());
+    let search_path = Path::new(&optee_client_dir).join("usr/lib");
     println!("cargo:rustc-link-search={}", search_path.display());
     println!("cargo:rustc-link-lib=dylib=teec");
 }
