693be179fa5691581286a914b44c7867f32a62c3
Ali Zhang <alizhang@google.com>
2024-05-07 14:21:41 -0700

    examples: Add error_handling-rs
    Add error_handling-rs to exercise some of the failure paths in the
    optee_rust stack.
    
    This initial commit exposes a double-free bug in optee-utee.
    
    Test: run `error_handling-rs` in the qemu environment described at
    https://optee.readthedocs.io/en/latest/building/optee_with_rust.html
    and observe the following output.
    
    thread 'main' panicked at src/main.rs:42:5:
    assertion `left == right` failed
      left: TargetDead
     right: Generic
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    101

---
+++ examples/error_handling-rs/host/src/main.rs
@@ -0,0 +1,43 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use optee_teec::{Context, ErrorKind, Operation, ParamType, Session, Uuid};
+use optee_teec::ParamNone;
+use proto::{UUID, Command};
+
+fn main() -> optee_teec::Result<()> {
+    test_error_handling();
+    Ok(())
+}
+
+fn test_error_handling() {
+    let mut ctx = Context::new().unwrap();
+    let uuid = Uuid::parse_str(UUID).unwrap();
+    let mut session = ctx.open_session(uuid).unwrap();
+    let mut operation = Operation::new(0, ParamNone, ParamNone, ParamNone, ParamNone);
+
+    // Test successful invocation return Ok().
+    session.invoke_command(Command::ReturnSuccess as u32, &mut operation).expect("success");
+
+    // Test error invocation returns the requested error.
+    let e = session.invoke_command(Command::ReturnGenericError as u32, &mut operation).expect_err("generic error");
+    assert_eq!(e.kind(), ErrorKind::Generic);
+
+    // Test repeated error invocation also returns the requested error.
+    let e = session.invoke_command(Command::ReturnGenericError as u32, &mut operation).expect_err("generic error");
+    assert_eq!(e.kind(), ErrorKind::Generic);
+}
---
+++ examples/error_handling-rs/ta/src/main.rs
@@ -0,0 +1,94 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#![no_std]
+#![no_main]
+#![feature(c_size_t)]
+
+extern crate alloc;
+
+use alloc::boxed::Box;
+use alloc::vec;
+use alloc::vec::Vec;
+
+use optee_utee::{
+    ta_close_session, ta_create, ta_destroy, ta_invoke_command, ta_open_session, trace_println,
+};
+use optee_utee::{Error, ErrorKind, Parameters, Result};
+use proto::Command;
+
+pub struct SessionContext {
+    stuff_on_heap: Vec<u8>,
+}
+
+impl Default for SessionContext {
+    fn default() -> Self {
+        Self {
+            stuff_on_heap: vec![1, 2, 3, 4],
+        }
+    }
+}
+
+#[ta_create]
+fn create() -> Result<()> {
+    trace_println!("[+] TA create");
+    Ok(())
+}
+
+#[ta_open_session]
+fn open_session(_params: &mut Parameters, _sess_ctx: &mut SessionContext) -> Result<()> {
+    trace_println!("[+] TA open session");
+    Ok(())
+}
+
+#[ta_close_session]
+fn close_session(_sess_ctx: &mut SessionContext) {
+    trace_println!("[+] TA close session");
+}
+
+#[ta_destroy]
+fn destroy() {
+    trace_println!("[+] TA destroy");
+}
+
+#[ta_invoke_command]
+fn invoke_command(
+    _sess_ctx: &mut SessionContext,
+    cmd_id: u32,
+    params: &mut Parameters,
+) -> Result<()> {
+    trace_println!("[+] TA invoke command");
+    match Command::from(cmd_id) {
+        Command::ReturnSuccess => Ok(()),
+        Command::ReturnGenericError => Err(Error::new(ErrorKind::Generic)),
+        _ => Err(Error::new(ErrorKind::NotSupported)),
+    }
+}
+
+// TA configurations
+const TA_FLAGS: u32 = 0;
+const TA_DATA_SIZE: u32 = 32 * 1024;
+const TA_STACK_SIZE: u32 = 2 * 1024;
+const TA_VERSION: &[u8] = b"0.1\0";
+const TA_DESCRIPTION: &[u8] = b"This is a TA for error handling tests.\0";
+const EXT_PROP_VALUE_1: &[u8] = b"Error handling TA\0";
+const EXT_PROP_VALUE_2: u32 = 0x0010;
+const TRACE_LEVEL: i32 = 4;
+const TRACE_EXT_PREFIX: &[u8] = b"TA\0";
+const TA_FRAMEWORK_STACK_SIZE: u32 = 2048;
+
+include!(concat!(env!("OUT_DIR"), "/user_ta_header.rs"));
