c9b418799eef6aa5bf740d3a7c5f9c8b9afee7eb
Yuan Zhuang <yuanz@apache.org>
2024-11-26 13:24:39 +0000

    projects: add basic eth wallet demo

---
+++ projects/web3/eth_wallet/host/src/main.rs
@@ -0,0 +1,113 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+mod cli;
+
+use optee_teec::{Context, Operation, ParamType, Uuid};
+use optee_teec::{ParamNone, ParamTmpRef, ParamValue};
+
+use anyhow::{bail, Result};
+use structopt::StructOpt;
+
+const OUTPUT_MAX_SIZE: usize = 1024;
+
+fn invoke_command(command: proto::Command, input: &[u8]) -> optee_teec::Result<Vec<u8>> {
+    let mut ctx = Context::new()?;
+    let uuid = Uuid::parse_str(proto::UUID)
+        .map_err(|_| optee_teec::Error::new(optee_teec::ErrorKind::ItemNotFound))?;
+    let mut session = ctx.open_session(uuid)?;
+
+    println!("CA: command: {:?}", command);
+    // input buffer
+    let p0 = ParamTmpRef::new_input(input);
+    // output buffer
+    let mut output = vec![0u8; OUTPUT_MAX_SIZE];
+    let p1 = ParamTmpRef::new_output(output.as_mut_slice());
+    // output buffer size
+    let p2 = ParamValue::new(0, 0, ParamType::ValueInout);
+
+    let mut operation = Operation::new(0, p0, p1, p2, ParamNone);
+    match session.invoke_command(command as u32, &mut operation) {
+        Ok(()) => {
+            println!("CA: invoke_command success");
+            let output_len = operation.parameters().2.a() as usize;
+            Ok(output[..output_len].to_vec())
+        }
+        Err(e) => {
+            let output_len = operation.parameters().2.a() as usize;
+            let err_message = String::from_utf8_lossy(&output[..output_len]);
+            println!("CA: invoke_command failed: {:?}", err_message);
+            Err(e)
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let args = cli::Opt::from_args();
+    match args.command {
+        cli::Command::CreateWallet(_opt) => {
+            let serialized_output = invoke_command(proto::Command::CreateWallet, &[])?;
+            let output: proto::CreateWalletOutput = bincode::deserialize(&serialized_output)?;
+            println!("Wallet ID: {}", output.wallet_id);
+        }
+        cli::Command::RemoveWallet(opt) => {
+            let input = proto::RemoveWalletInput {
+                wallet_id: opt.wallet_id,
+            };
+            let _output =
+                invoke_command(proto::Command::RemoveWallet, &bincode::serialize(&input)?)?;
+            println!("Wallet removed");
+        }
+        cli::Command::DeriveAddress(opt) => {
+            let input = proto::DeriveAddressInput {
+                wallet_id: opt.wallet_id,
+                hd_path: opt.hd_path,
+            };
+            let serialized_output =
+                invoke_command(proto::Command::DeriveAddress, &bincode::serialize(&input)?)?;
+            let output: proto::DeriveAddressOutput = bincode::deserialize(&serialized_output)?;
+            println!("Address: 0x{}", hex::encode(&output.address));
+            println!("Public key: {}", hex::encode(&output.public_key));
+        }
+        cli::Command::SignTransaction(opt) => {
+            let transaction = proto::EthTransaction {
+                chain_id: opt.chain_id,
+                nonce: opt.nonce,
+                to: Some(opt.to),
+                value: opt.value,
+                gas_price: opt.gas_price,
+                gas: opt.gas,
+                data: vec![],
+            };
+            let input = proto::SignTransactionInput {
+                wallet_id: opt.wallet_id,
+                hd_path: opt.hd_path,
+                transaction,
+            };
+            let serialized_output = invoke_command(
+                proto::Command::SignTransaction,
+                &bincode::serialize(&input)?,
+            )?;
+            let output: proto::SignTransactionOutput = bincode::deserialize(&serialized_output)?;
+            println!("Signature: {}", hex::encode(&output.signature));
+        }
+        _ => {
+            bail!("Unsupported command");
+        }
+    }
+    Ok(())
+}
---
+++ projects/web3/eth_wallet/ta/build.rs
@@ -0,0 +1,108 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use proto;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::path::{Path, PathBuf};
+use uuid::Uuid;
+
+fn main() -> std::io::Result<()> {
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    let mut buffer = File::create(out.join("user_ta_header.rs"))?;
+    buffer.write_all(include_bytes!("ta_static.rs"))?;
+
+    let tee_uuid = Uuid::parse_str(proto::UUID).unwrap();
+    let (time_low, time_mid, time_hi_and_version, clock_seq_and_node) = tee_uuid.as_fields();
+
+    write!(buffer, "\n")?;
+    write!(
+        buffer,
+        "const TA_UUID: optee_utee_sys::TEE_UUID = optee_utee_sys::TEE_UUID {{
+    timeLow: {:#x},
+    timeMid: {:#x},
+    timeHiAndVersion: {:#x},
+    clockSeqAndNode: {:#x?},
+}};",
+        time_low, time_mid, time_hi_and_version, clock_seq_and_node
+    )?;
+
+    let mut aarch64_flag = true;
+    match env::var("TARGET_TA") {
+        Ok(ref v) if v == "arm-unknown-linux-gnueabihf" || v == "arm-unknown-optee" => {
+            println!("cargo:rustc-link-arg=--no-warn-mismatch");
+            aarch64_flag = false;
+        }
+        _ => {}
+    };
+
+    let optee_os_dir = env::var("TA_DEV_KIT_DIR").unwrap();
+    let search_path = Path::new(&optee_os_dir).join("lib");
+
+    let optee_os_path = &PathBuf::from(optee_os_dir.clone());
+    let mut ta_lds = File::create(out.join("ta.lds"))?;
+    let f = File::open(optee_os_path.join("src/ta.ld.S"))?;
+    let f = BufReader::new(f);
+
+    for line in f.lines() {
+        let l = line?;
+
+        if aarch64_flag {
+            if l.starts_with('#')
+                || l == "OUTPUT_FORMAT(\"elf32-littlearm\")"
+                || l == "OUTPUT_ARCH(arm)"
+            {
+                continue;
+            }
+        } else {
+            if l.starts_with('#')
+                || l == "OUTPUT_FORMAT(\"elf64-littleaarch64\")"
+                || l == "OUTPUT_ARCH(aarch64)"
+            {
+                continue;
+            }
+        }
+
+        if l == "\t. = ALIGN(4096);" {
+            write!(ta_lds, "\t. = ALIGN(65536);\n")?;
+        } else {
+            write!(ta_lds, "{}\n", l)?;
+        }
+    }
+
+    println!("cargo:rustc-link-search={}", out.display());
+    println!("cargo:rerun-if-changed=ta.lds");
+
+    println!("cargo:rustc-link-search={}", search_path.display());
+    println!("cargo:rustc-link-lib=static=utee");
+    println!("cargo:rustc-link-lib=static=utils");
+    println!("cargo:rustc-link-arg=-Tta.lds");
+    println!("cargo:rustc-link-arg=-e__ta_entry");
+    println!("cargo:rustc-link-arg=-pie");
+    println!("cargo:rustc-link-arg=-Os");
+    println!("cargo:rustc-link-arg=--sort-section=alignment");
+
+    let mut dyn_list = File::create(out.join("dyn_list"))?;
+    write!(
+        dyn_list,
+        "{{ __elf_phdr_info; trace_ext_prefix; trace_level; ta_head; }};\n"
+    )?;
+    println!("cargo:rustc-link-arg=--dynamic-list=dyn_list");
+    Ok(())
+}
