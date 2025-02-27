e12359b9612a4a9bf1bd345fe3d34cf49eac2a6b
Yuan Zhuang <yuanz@apache.org>
2025-02-27 10:25:40 +0000

    examples: Add secure_db_abstraction (std-only)
    Add reference implementation for TA that simplifies interaction
    with secure storage. It provides basic methods for database
    operations, including `get()`, `put()`, `delete_entries()`, and
    `list_entries()`, making it easier for developers to store and
    retrieve data based on Rust Type constraints.
    
    The example is std-only for now.
    
    Signed-off-by: Yuan Zhuang <yuanz@apache.org>
    Reviewed-by: Zehui Chen <ivila@apache.org>

---
+++ examples/secure_db_abstraction-rs/ta/src/secure_db/storable.rs
@@ -0,0 +1,52 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::{convert::TryFrom, hash::Hash};
+
+// For each key-value data, the storage key is "$TABLE_NAME#$KEY"
+// For example, if we store the Data whose type is Structure named
+// ExampleData, with id "123",
+// the storage key will be "ExampleData#123"
+
+const CONCAT: &str = "#";
+
+pub trait Storable {
+    type Key: Into<String> + Clone + TryFrom<String> + Eq + Hash; // Associated type `Key`
+
+    fn unique_id(&self) -> Self::Key;
+
+    fn table_name() -> &'static str {
+        // keeps the last part of the path
+        std::any::type_name::<Self>()
+            .split("::")
+            .last()
+            .unwrap_or("WRONG_TABLE_NAME")
+    }
+
+    fn storage_key(&self) -> String {
+        format!(
+            "{}{}{}",
+            Self::table_name(),
+            CONCAT,
+            Into::<String>::into(self.unique_id())
+        )
+    }
+
+    fn concat_key(key: &str) -> String {
+        format!("{}{}{}", Self::table_name(), CONCAT, key)
+    }
+}
