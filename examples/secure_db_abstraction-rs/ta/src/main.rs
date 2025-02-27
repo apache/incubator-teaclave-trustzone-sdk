dc339962c5558aa5629c1b73d1d0a1410487e360
Yuan Zhuang <yuanz@apache.org>
2025-02-27 10:25:40 +0000

    examples: Add secure_db_abstraction (std-only)
    Add reference implementation for TA that simplifies interaction
    with secure storage. It provides basic methods for database
    operations, including `get()`, `put()`, `delete_entries()`, and
    `list_entries()`, making it easier for developers to store and
    retrieve data based on Rust Type constraints.
    
    The example is std-only for now.
    
    Signed-off-by: Yuan Zhuang <yuanz@apache.org>
    Reviewed-by: Zehui Chen <ivila@apache.org>

---
+++ examples/secure_db_abstraction-rs/ta/src/main.rs
@@ -0,0 +1,120 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#![no_main]
+
+mod secure_db;
+
+extern crate alloc;
+
+use alloc::vec;
+use optee_utee::{
+    ta_close_session, ta_create, ta_destroy, ta_invoke_command, ta_open_session, trace_println,
+};
+use optee_utee::{Error, ErrorKind, Parameters, Result};
+use proto::Command;
+use secure_db::{SecureStorageClient, Storable};
+use serde::{Deserialize, Serialize};
+
+#[ta_create]
+fn create() -> Result<()> {
+    trace_println!("[+] TA create");
+    Ok(())
+}
+
+#[ta_open_session]
+fn open_session(_params: &mut Parameters) -> Result<()> {
+    trace_println!("[+] TA open session");
+    Ok(())
+}
+
+#[ta_close_session]
+fn close_session() {
+    trace_println!("[+] TA close session");
+}
+
+#[ta_destroy]
+fn destroy() {
+    trace_println!("[+] TA destroy");
+}
+
+#[ta_invoke_command]
+fn invoke_command(cmd_id: u32, _params: &mut Parameters) -> Result<()> {
+    trace_println!("[+] TA invoke command");
+    match Command::from(cmd_id) {
+        Command::Test => match test() {
+            Ok(_) => {
+                trace_println!("[+] Test passed");
+                Ok(())
+            }
+            Err(e) => {
+                trace_println!("[-] Test failed: {:?}", e);
+                Err(Error::new(ErrorKind::Generic))
+            }
+        },
+        _ => {
+            return Err(Error::new(ErrorKind::NotSupported));
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct ExampleData {
+    pub id: String,
+    pub data: Vec<u8>,
+}
+
+// Any structure that implements Storable can be stored in the secure db.
+// Any Key type can be used as unique id as long as it implements
+// TryFrom<String> + Into<String> + Clone
+impl Storable for ExampleData {
+    type Key = String;
+
+    fn unique_id(&self) -> Self::Key {
+        self.id.clone()
+    }
+}
+
+pub fn test() -> anyhow::Result<()> {
+    // Assume this is the data that we want to store
+    let example_data = ExampleData {
+        id: "example_data".to_string(),
+        data: vec![1, 2, 3, 4, 5],
+    };
+
+    // Initialize secure storage db client with a db name
+    let db_client = SecureStorageClient::open("secure_db")?;
+
+    // Now, we can do common db interactions using the db_client:
+    // Store data in db using put()
+    db_client.put(&example_data)?;
+    // Load data from db using get()
+    let loaded_example_data = db_client.get::<ExampleData>(&example_data.id)?;
+    anyhow::ensure!(
+        loaded_example_data == example_data,
+        "Loaded example_data is not equal to the original example_data"
+    );
+    // List all entries in db
+    let entries = db_client.list_entries::<ExampleData>()?;
+    trace_println!("Entries: {:?}", entries);
+    // Delete entry from db
+    db_client.delete_entry::<ExampleData>(&example_data.id)?;
+
+    Ok(())
+}
+
+include!(concat!(env!("OUT_DIR"), "/user_ta_header.rs"));
---
+++ examples/secure_db_abstraction-rs/ta/src/secure_db/db.rs
@@ -0,0 +1,113 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use crate::secure_db::{
+    delete_from_secure_storage, load_from_secure_storage, save_in_secure_storage,
+};
+use anyhow::{bail, ensure, Result};
+use std::collections::{HashMap, HashSet};
+
+// SecureStorageDb is a key-value storage for TA to easily store and retrieve data.
+// First we store the key list in the secure storage, named as db_name.
+// Then we store the each key-value pairs in the secure storage.
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SecureStorageDb {
+    name: String,
+    key_list: HashSet<String>,
+}
+
+impl SecureStorageDb {
+    pub fn open(name: String) -> Result<Self> {
+        match load_from_secure_storage(name.as_bytes())? {
+            Some(data) => {
+                let key_list = bincode::deserialize(&data)?;
+                Ok(Self { name, key_list })
+            }
+            None => {
+                // create new db
+                Ok(Self {
+                    name,
+                    key_list: HashSet::new(),
+                })
+            }
+        }
+    }
+
+    pub fn put(&mut self, key: String, value: Vec<u8>) -> Result<()> {
+        match save_in_secure_storage(key.as_bytes(), &value) {
+            Ok(_) => {
+                self.key_list.insert(key);
+                self.store_key_list()?;
+            }
+            Err(e) => {
+                bail!("[+] SecureStorage::insert(): save error: {}", e);
+            }
+        };
+        Ok(())
+    }
+
+    pub fn get(&self, key: &str) -> Result<Vec<u8>> {
+        ensure!(self.key_list.contains(key), "Key not found in key list");
+        match load_from_secure_storage(key.as_bytes()) {
+            Ok(Some(data)) => Ok(data),
+            Ok(None) => bail!("[+] SecureStorage::get(): object not found in db"),
+            Err(e) => {
+                bail!("[+] SecureStorage::get(): load error: {}", e);
+            }
+        }
+    }
+
+    pub fn delete(&mut self, key: &str) -> Result<()> {
+        // ensure key must exist
+        ensure!(self.key_list.contains(key), "Key not found in key list");
+        match delete_from_secure_storage(key.as_bytes()) {
+            Ok(_) => {
+                self.key_list.remove(key);
+                self.store_key_list()?;
+            }
+            Err(e) => {
+                bail!("[+] SecureStorage::delete(): delete error: {}", e);
+            }
+        };
+        Ok(())
+    }
+
+    pub fn clear(&mut self) -> Result<()> {
+        for key in self.key_list.clone() {
+            self.delete(&key)?;
+        }
+        Ok(())
+    }
+
+    pub fn list_entries_with_prefix(&self, prefix: &str) -> Result<HashMap<String, Vec<u8>>> {
+        let mut result = HashMap::new();
+        for key in &self.key_list {
+            if key.starts_with(prefix) {
+                let value = self.get(key)?;
+                result.insert(key.clone(), value);
+            }
+        }
+        Ok(result)
+    }
+
+    fn store_key_list(&self) -> Result<()> {
+        let key_list = bincode::serialize(&self.key_list)?;
+        save_in_secure_storage(self.name.as_bytes(), &key_list)?;
+        Ok(())
+    }
+}
